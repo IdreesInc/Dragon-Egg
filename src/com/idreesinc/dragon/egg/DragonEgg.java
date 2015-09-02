f7c500aa13f25c4779747bce332d62c5a32dd97d
Idrees Hassan <idrees@idreesinc.com>
2015-09-01 20:43:33 -0400

    Completed teleporting functionality.
    The Dragon_Egg.jar can now teleport between files, going wherever it
    dang well pleases, up to the maximum vertical distance specified.

--- src/com/idreesinc/dragon/egg/DragonEgg.java
+++ src/com/idreesinc/dragon/egg/DragonEgg.java
@@ -1,21 +1,46 @@
 package com.idreesinc.dragon.egg;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class DragonEgg {
 
+    private static final String SEPERATOR = File.separator;
+    private static final int MAX_DISTANCE = 15;
+
     public static void main(String[] args) {
         String path = DragonEgg.class.getProtectionDomain().getCodeSource().getLocation().toString().replace("file:", "").replace("%20", " ");
+        Random rand = new Random();
+        int folderLevel = path.length() - path.replace(SEPERATOR, "").length();
+        int newFolderLevel = folderLevel;
+        File newLocation = new File(System.getProperty("user.dir"));
+        for (int y = 0; y < MAX_DISTANCE; y++) {
+            if (rand.nextBoolean() && newFolderLevel > 3) {
+                newLocation = newLocation.getParentFile();
+                newFolderLevel--;
+            } else {
+                String subdirectory = getFolderInPath(newLocation.getAbsolutePath());
+                if (subdirectory != null) {
+                    newFolderLevel++;
+                    newLocation = new File(subdirectory);
+                }
+            }
+        }
+        if (!newLocation.getPath().contains("Dragon_Egg.jar")) {
+            newLocation = new File(newLocation.getAbsolutePath() + SEPERATOR + "Dragon_Egg.jar");
+        }
         if (path.contains("jar")) {
             try {
-                Files.move(Paths.get(path), Paths.get(path.replace("Dragon_Egg.jar", "Banana/Dragon_Egg.jar")), REPLACE_EXISTING);
-                say("Poof");
+                Files.move(Paths.get(path), newLocation.toPath(), REPLACE_EXISTING);
+                say("*Poof*" + "\nPssst: " + newLocation.getAbsolutePath());
             } catch (IOException ex) {
                 Logger.getLogger(DragonEgg.class.getName()).log(Level.SEVERE, null, ex);
                 say("Error: " + ex.toString());
@@ -23,7 +48,24 @@ public class DragonEgg {
         }
     }
 
+    public static String getFolderInPath(String path) {
+        File file = new File(path);
+        String[] names = file.list();
+        ArrayList<String> folders = new ArrayList<>();
+        for (String name : names) {
+            if (new File(path + SEPERATOR + name).isDirectory() && !name.contains("%")) {
+                folders.add(path + SEPERATOR + name);
+            }
+        }
+        if (!folders.isEmpty()) {
+            Random rand = new Random();
+            return folders.get(rand.nextInt(folders.size()));
+        } else {
+            return null;
+        }
+    }
+
     private static void say(String message) {
-        JOptionPane.showMessageDialog(null, message, "Output", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showMessageDialog(null, message, "Dragon Egg", JOptionPane.INFORMATION_MESSAGE);
     }
 }
